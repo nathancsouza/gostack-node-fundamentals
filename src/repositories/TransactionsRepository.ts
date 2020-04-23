a49b1f635d832f756c19801d376c3196e2d346e9
Nathan Souza <nathancsouza@gmail.com>
2020-04-23 00:49:31 -0700

    challenge complete

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface RequestDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,42 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const { income, outcome } = this.transactions.reduce(
+      (counter: Balance, indexTransaction: Transaction) => {
+        switch (indexTransaction.type) {
+          case 'income':
+            counter.income += indexTransaction.value;
+            break;
+          case 'outcome':
+            counter.outcome += indexTransaction.value;
+            break;
+          default:
+            break;
+        }
+        return counter;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    const total = income - outcome;
+
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: RequestDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
